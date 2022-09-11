2ca1f24fa10b2a8ce237cf2eb315bc151f516e1f
unknown <berkozzambak@gmail.com>
2022-09-11 18:16:23 +0200

    Add database connection and environment variables

---
+++ server/database/database.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const database = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+database.connect((err) => {
+  if (err) {
+    return console.error("Error:" + err.message);
+  }
+  console.log("Connected to MySQL server");
+});
+async function getUsers() {
+  const [users] = await database.query("SELECT * FROM Users");
+  return users;
+}
+
+module.export = getUsers;
--- server/index.js
+++ server/index.js
@@ -1,27 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mysql = require("mysql");
+const getUsers = require("./database/database");
 
 const app = express();
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "webapp",
-  password: "WebAppPass1!",
-  database: "my-word-space",
-});
-
 app.use(bodyParser.json());
-connection.connect((err) => {
-  if (err) {
-    return console.error("Error:" + err.message);
-  }
 
-  console.log("Connected to MySQL server");
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/", async (req, res) => {
+  const users = await getUsers();
+  res.send(users);
 });
 
 //Handle Production
