33985221574cffe545a1d7c59c2532cb8439f846
unknown <berkozzambak@gmail.com>
2022-10-29 20:28:53 +0200

    Add missing fields error to add-new-deck route

--- server/routes/decks.js
+++ server/routes/decks.js
@@ -17,11 +17,15 @@ module.exports = function (database) {
     // res.status(200).json(decks);
   };
 
-//   const addNewDeck = async (req, res) => {
-//     //parse the request
-//     // pass it to the db function
-//     //return result
-//   };
+  const addNewDeck = async (req, res) => {
+    if (!req.body.userId || !req.body.deckName) {
+        res.status(400).json(missingFieldsError);
+        return;
+      }
+    //parse the request
+    // pass it to the db function
+    //return result
+  };
 
 //   const editDeck = async (req, res) => {
 //     //parse the request
@@ -36,7 +40,7 @@ module.exports = function (database) {
 //   };
 
   router.get("/get-all", getAllDecks);
-//   router.post("/add", addNewDeck);
+  router.post("/add", addNewDeck);
 //   router.post("/edit", editDeck);
 //   router.delete("/delete", deleteDeck);
 
