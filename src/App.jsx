15d8caf1feb522d8c9a5a5d308f92b395c97688f
unknown <berkozzambak@gmail.com>
2022-11-10 19:18:30 +0100

    Add router to the app structure and import the views

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Exercise from "./Views/Exercise";
+import Word from "./Views/Word";
+import Auth from "./Views/Auth";
+import Dashboard from "./Views/Dashboard";
+import Notebook from "./Views/Notebook";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Stripped down my word space</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={Auth} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/exercise" component={Exercise} />
+        <Route path="/word" component={Word} />
+        <Route path="/notebook" component={Notebook} />
+      </Switch>
+    </Router>
   );
 }
 
