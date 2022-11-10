b055677da41eefa6629a03e22c5b6e76967466c3
unknown <berkozzambak@gmail.com>
2022-11-10 18:23:08 +0100

    Initial commit of the front end

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Stripped down my word space</h1>
+    </div>
+  );
+}
+
+export default App;
