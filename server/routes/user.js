a521819ad0bf418aced485e4f13f7c0013ae9221
unknown <berkozzambak@gmail.com>
2022-11-17 11:00:10 +0100

    Add error handling and logging to begin exercise service

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,25 +1,18 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const findUsers = require('../helpers/getUser')
+const findUsers = require("../helpers/getUser");
 
-
-module.exports = function(){
-  const findUser = async(req, res) => {
-    
-    try{
-      let user = await findUsers.byUsername('tezst-user')
-      res.status(200).json(user)
-    }catch(err){
-      res.status(500).send('Non')
+module.exports = function () {
+  const findUser = async (req, res) => {
+    try {
+      let user = await findUsers.byUsername("tezst-user");
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).send("Non");
     }
-   
-  }
+  };
 
   router.get("/", findUser);
 
-  return router
-}
-
-
-
-
+  return router;
+};
