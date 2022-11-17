64612f9c4401e73f7b235608a67cc1a25f575c87
unknown <berkozzambak@gmail.com>
2022-11-17 17:17:25 +0100

    Update .envFileTemplate after the AWS SDK related naming changes

--- server/envFileTemplate.js
+++ server/envFileTemplate.js
@@ -8,7 +8,7 @@ MYSQL_DATABASE =
 JWT_SECRET = 
 --Cloudwatch variables are not required for running the app locally and can be left empty--
 CLOUDWATCH_GROUP_NAME = 
-CLOUDWATCH_ACCESS_KEY = 
-CLOUDWATCH_SECRET_ACCESS_KEY = 
+AWS_ACCESS_KEY_ID = 
+AWS_SECRET_ACCESS_KEY = 
 CLOUDWATCH_REGION = 
 */
