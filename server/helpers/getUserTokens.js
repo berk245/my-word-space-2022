e4c08a263b19c2ec25181a251c485fc4d8e5dd15
unknown <berkozzambak@gmail.com>
2022-11-17 10:31:21 +0100

    Add error logging to token creation

--- server/helpers/getUserTokens.js
+++ server/helpers/getUserTokens.js
@@ -1,14 +1,19 @@
 const jwt = require("jsonwebtoken");
+const CloudWatch = require("../config/logger");
 module.exports = async function getUserTokens(user) {
   try {
-    const accessToken = jwt.sign({userId: user.UserID}, process.env.JWT_SECRET, {expiresIn :  '3d'});
-    const refreshToken = jwt.sign(
-      user.UserID,
-      process.env.REFRESH_TOKEN_SECRET
+    const accessToken = jwt.sign(
+      { userId: user.UserID },
+      process.env.JWT_SECRET,
+      { expiresIn: "3d" }
     );
-
-    return { accessToken: accessToken, refreshToken: refreshToken };
+    return { accessToken: accessToken };
   } catch (err) {
+    CloudWatch.log(
+      "error",
+      `Error while creating user token:${err}`,
+      `userId: ${user.userId}`
+    );
     return false;
   }
 };
